f92b39151fd16a2db9b93b6b869beb8f2a4e0b04
Johannes Weber <johannes@drweber.de>
2022-05-07 15:00:43 -0700

    adjust for simplified schema

--- src/modules/resources.js
+++ src/modules/resources.js
@@ -2,70 +2,19 @@ const helpers = require("../modules/helpers");
 
 module.exports = (db) => {
 
-  /**
-   * Get the names of the authors for a resource
-   * @param {integer} resourceId
-   * @returns an array of author's names
-   */
-  const getAuthors = (resourceId) => {
-    return new Promise((resolve, reject) => {
-      const query = {
-        text: `SELECT name FROM authors JOIN authors_resources ON authors.id = authors_resources.author_id WHERE resource_id = $1`,
-        values: [ resourceId ]
-      };
-
-      db.query(query)
-        .then(({ rows: records }) => {
-          const authors = [];
-          records.forEach(record => {
-            authors.push(record.name);
-          });
-          resolve(authors);
-        })
-        .catch(err => reject(err));
-    });
-  };
-
-  /**
-   * Get the genres for a resource
-   * @param {integer} resourceId
-   * @returns an array of genre names
-   */
-  const getGenres = (resourceId) => {
-    return new Promise((resolve, reject) => {
-      const query = {
-        text: `SELECT name FROM genres JOIN genres_resources ON genres.id = genres_resources.genre_id WHERE resource_id = $1`,
-        values: [ resourceId ]
-      };
-
-      db.query(query)
-        .then(({ rows: records }) => {
-          const genres = [];
-          records.forEach(record => {
-            genres.push(record.name);
-          });
-          resolve(genres);
-        })
-        .catch(err => reject(err));
-    });
-  };
-
+  
   /**
    * Get an object with all information on the resource, including author- and genre-arrays
    * @param {integer} resourceId
    * @returns an object with resource information
    */
-  const getResourceObject = (resourceId) => {
+  const getOne = (resourceId) => {
     return new Promise((resolve, reject) => {
-      db.query(`SELECT * FROM resources WHERE id = $1::integer`, [ resourceId ])
+      db.query(`SELECT * FROM resources WHERE id = $1::integer`, [ helpers.sanitizeId(resourceId) ])
         .then(({ rows: records }) => {
           if (records.length > 0) {
             const record = records[0];
-            Promise.all([getAuthors(resourceId), getGenres(resourceId)]).then(([authors, genres]) => {
-              record.authors = authors;
-              record.genres = genres;
-              resolve(record);
-            });
+            resolve(record);
           } else {
             resolve({});
           }
@@ -87,10 +36,10 @@ module.exports = (db) => {
       where += ` AND LOWER(resources.title) LIKE '%${helpers.lowerAndEscape(query.title)}%'`;
     }
     if (query.author) {
-      where += ` AND LOWER(authors.name) LIKE '%${helpers.lowerAndEscape(query.author)}%'`;
+      where += ` AND LOWER(resources.authors) LIKE '%${helpers.lowerAndEscape(query.author)}%'`;
     }
     if (query.genre) {
-      where += ` AND LOWER(genres.name) LIKE '%${helpers.lowerAndEscape(query.genre)}%'`;
+      where += ` AND LOWER(resources.genres) LIKE '%${helpers.lowerAndEscape(query.genre)}%'`;
     }
     if (query.owner_id) {
       where += ` AND resources.owner_id = ${helpers.sanitizeId(query.owner_id)}`;
@@ -121,13 +70,8 @@ module.exports = (db) => {
     const limit = query.limit && query.limit > 0 ? `LIMIT ${query.limit}` : ``;
 
     let q = `
-    SELECT DISTINCT resources.title, resources.id, resources.created_at
-    FROM authors 
-    JOIN authors_resources ON authors_resources.author_id = authors.id 
-    JOIN resources ON authors_resources.resource_id = resources.id
-    JOIN genres_resources ON genres_resources.resource_id = resources.id
-    JOIN genres ON genres_resources.genre_id = genres.id
-    ${where} ${order} ${limit}
+      SELECT * FROM resources
+      ${where} ${order} ${limit}
     `;
     return q;
   };
@@ -135,36 +79,18 @@ module.exports = (db) => {
   /**
    * Get data for all resources; order by title or most recent additions
    * @param {object} object containing orderby and limit properties
-   * @returns an array with resource information objects
+   * @returns an array with resource objects
    */
   const getAll = (query) => {
     return new Promise((resolve) => {
       const queryString = buildResourceSearchQuery(query);
       db.query(queryString)
         .then(({ rows: resources }) => {
-          const promises = [];
-          resources.forEach(resource => {
-            promises.push(getResourceObject(resource.id));
-          });
-          Promise.all(promises).then(resources => {
-            resolve(resources);
-          });
+          resolve(resources);
         });
     });
   };
-
-  /**
-   * Create a resource record with given data,
-   * return promise to record id
-   * @param {object} data
-   * @returns a promise to id of new record
-   */
-  const createResourceRecord = (data) => {
-    return new Promise((resolve, reject) => {
-      resolve();
-    });
-  };
-
+  
   /**
    * Take data from post request and create new resource record
    * @param {object} reqBody
@@ -178,7 +104,7 @@ module.exports = (db) => {
 
   return {
     getAll,
-    getOne: getResourceObject,
+    getOne,
     createNew,
   };
   
