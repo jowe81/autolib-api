95c928132f3c170a967855f906570d0da39388b7
Johannes Weber <johannes@drweber.de>
2022-05-06 20:23:00 -0700

    copy from development.sql

--- src/db/schema/production.sql
+++ src/db/schema/production.sql
@@ -1,39 +1,82 @@
-WITH days(day) AS (
-  VALUES ( 'Monday' ), ( 'Tuesday' ), ( 'Wednesday' ), ( 'Thursday' ), ( 'Friday' )
-)
-INSERT INTO days (name)
-SELECT day FROM days;
+INSERT INTO users (first_name, last_name, email, street_address, zip_code, city, province, country)
+VALUES ('Stephanie', 'Smith', 'stephanie@gmail.com', '356 10th Avenue Bikeway', 'V5Y 1S3', 'Vancouver', 'BC', 'Canada');
 
-WITH times(time) AS (
-	VALUES ('12pm'), ('1pm'), ('2pm'), ('3pm'), ('4pm')
-)
-INSERT INTO appointments (time, day_id)
-SELECT time, id as day_id FROM days, times ORDER BY day_id, time;
+INSERT INTO users (first_name, last_name, email, street_address, zip_code, city, province, country)
+VALUES ('Mark', 'Spencer', 'mark@gmail.com', '2812 Alberta St', 'V5Y 1T8', 'Vancouver', 'BC', 'Canada');
 
-INSERT INTO interviewers (name, avatar)
-VALUES
-  ('Sylvia Palmer', 'https://i.imgur.com/LpaY82x.png'),
-  ('Tori Malcolm', 'https://i.imgur.com/Nmx0Qxo.png'),
-  ('Mildred Nazir', 'https://i.imgur.com/T2WwVfS.png'),
-  ('Cohana Roy', 'https://i.imgur.com/FK8V841.jpg'),
-  ('Sven Jones', 'https://i.imgur.com/twYrpay.jpg'),
-  ('Susan Reynolds', 'https://i.imgur.com/TdOAdde.jpg'),
-  ('Alec Quon', 'https://i.imgur.com/3tVgsra.jpg'),
-  ('Viktor Jain', 'https://i.imgur.com/iHq8K8Z.jpg'),
-  ('Lindsay Chu', 'https://i.imgur.com/nPywAp1.jpg'),
-  ('Samantha Stanic', 'https://i.imgur.com/okB9WKC.jpg');
+INSERT INTO users (first_name, last_name, email, street_address, zip_code, city, province, country)
+VALUES ('Tanya', 'Shoemaker', 'tanya@gmail.com', '106 E 18th Ave', 'V5V 1E3', 'Vancouver', 'BC', 'Canada');
 
-INSERT INTO available_interviewers (day_id, interviewer_id)
-SELECT 1 as day_id, interviewers.interviewer_id FROM ( SELECT id AS interviewer_id FROM interviewers ORDER BY RANDOM() LIMIT 5 ) interviewers;
+INSERT INTO users (first_name, last_name, email, street_address, zip_code, city, province, country)
+VALUES ('Ryan', 'Sayson', 'ryan@gmail.com', '3616 W 3rd Ave', 'V6R 1L9', 'Vancouver', 'BC', 'Canada');
 
-INSERT INTO available_interviewers (day_id, interviewer_id)
-SELECT 2 as day_id, interviewers.interviewer_id FROM ( SELECT id AS interviewer_id FROM interviewers ORDER BY RANDOM() LIMIT 5 ) interviewers;
+INSERT INTO users (first_name, last_name, email, street_address, zip_code, city, province, country)
+VALUES ('John', 'Chung', 'john@gmail.com', '2005 W 43rd Ave', 'V6M 2C8', 'Vancouver', 'BC', 'Canada');
 
-INSERT INTO available_interviewers (day_id, interviewer_id)
-SELECT 3 as day_id, interviewers.interviewer_id FROM ( SELECT id AS interviewer_id FROM interviewers ORDER BY RANDOM() LIMIT 5 ) interviewers;
+INSERT INTO users (first_name, last_name, email, street_address, zip_code, city, province, country)
+VALUES ('Lyndon', 'Mariner', 'lyndon@gmail.com', '4888 Selkirk St', 'V6H 3A3', 'Vancouver', 'BC', 'Canada');
 
-INSERT INTO available_interviewers (day_id, interviewer_id)
-SELECT 4 as day_id, interviewers.interviewer_id FROM ( SELECT id AS interviewer_id FROM interviewers ORDER BY RANDOM() LIMIT 5 ) interviewers;
+INSERT INTO users (first_name, last_name, email, street_address, zip_code, city, province, country)
+VALUES ('Mariah', 'MacLeod', 'mariah@gmail.com', '1225 Nanton Ave', 'V6H 2C7', 'Vancouver', 'BC', 'Canada');
 
-INSERT INTO available_interviewers (day_id, interviewer_id)
-SELECT 5 as day_id, interviewers.interviewer_id FROM ( SELECT id AS interviewer_id FROM interviewers ORDER BY RANDOM() LIMIT 5 ) interviewers;
+INSERT INTO users (first_name, last_name, email, street_address, zip_code, city, province, country)
+VALUES ('Carolin', 'McDonald', 'carolin@gmail.com', '1255 Devonshire Crescent', 'V6H 2G2', 'Vancouver', 'BC', 'Canada');
+
+
+
+
+INSERT INTO resources (isbn, title, description, cover_image, current_possessor_id, owner_id, status)
+VALUES ('9780261102439','The Lord of the Rings','The most influential fantasy novel ever written.','https://covers.openlibrary.org/b/isbn/9780261102439-M.jpg',1,2,'in use');
+
+INSERT INTO resources (isbn, title, description, cover_image, current_possessor_id, owner_id, status)
+VALUES ('9780596517748','Javascript: The Good Parts','JavaScript, having been developed and released in a hurry before it could be refined, has more than its share of the bad parts. This book scrapes away these bad features to reveal a JavaScript subset that is more reliable, readable, and maintainable than the language as a whole-a subset you can use to create extensible and efficient code.','https://covers.openlibrary.org/b/isbn/9780596517748-M.jpg',4,1,'available');
+
+INSERT INTO resources (isbn, title, description, cover_image, current_possessor_id, owner_id, status)
+VALUES ('9781449399023','JavaScript & jQuery: The Missing Manual','Javascript lets you supercharge your HTML with animation, interactivity, and visual effects. You will soon be building web pages that feel and act like desktop programs, without a lot of programming.','https://covers.openlibrary.org/b/isbn/9781449399023-M.jpg',3,3,'available');
+
+INSERT INTO resources (isbn, title, description, cover_image, current_possessor_id, owner_id, status)
+VALUES ('9781449302399','HTML5: The Missing Manual','HTML5 is more than a markup language. It is the future of the Web, and this book will get you there.','https://covers.openlibrary.org/b/isbn/9781449302399-M.jpg',2,3,'requested');
+
+INSERT INTO authors (name) VALUES ('J.R.R. Tolkien');
+INSERT INTO authors (name) VALUES ('Ian Holm');
+INSERT INTO authors (name) VALUES ('John Le Mesurier');
+INSERT INTO authors (name) VALUES ('Michael Hordern');
+INSERT INTO authors (name) VALUES ('Peter Woodthorpe');
+INSERT INTO authors (name) VALUES ('Robert Stephens');
+INSERT INTO authors (name) VALUES ('Douglas Crockford');
+INSERT INTO authors (name) VALUES ('David Sawyer McFarland');
+INSERT INTO authors (name) VALUES ('Matthew MacDonald');
+
+INSERT INTO authors_resources (resource_id, author_id) VALUES (1,1);
+INSERT INTO authors_resources (resource_id, author_id) VALUES (1,2);
+INSERT INTO authors_resources (resource_id, author_id) VALUES (1,3);
+INSERT INTO authors_resources (resource_id, author_id) VALUES (1,4);
+INSERT INTO authors_resources (resource_id, author_id) VALUES (1,5);
+INSERT INTO authors_resources (resource_id, author_id) VALUES (1,6);
+INSERT INTO authors_resources (resource_id, author_id) VALUES (2,7);
+INSERT INTO authors_resources (resource_id, author_id) VALUES (3,8);
+INSERT INTO authors_resources (resource_id, author_id) VALUES (4,9);
+
+INSERT INTO genres (name) VALUES ('Fantasy');
+INSERT INTO genres (name) VALUES ('Thriller');
+INSERT INTO genres (name) VALUES ('Science-Fiction');
+INSERT INTO genres (name) VALUES ('Non-fiction');
+INSERT INTO genres (name) VALUES ('Biography');
+INSERT INTO genres (name) VALUES ('Romance');
+INSERT INTO genres (name) VALUES ('Programming');
+INSERT INTO genres (name) VALUES ('JavaScript');
+INSERT INTO genres (name) VALUES ('jQuery');
+INSERT INTO genres (name) VALUES ('HTML');
+
+INSERT INTO genres_resources (resource_id, genre_id) VALUES (1,1);
+
+INSERT INTO genres_resources (resource_id, genre_id) VALUES (2,7);
+INSERT INTO genres_resources (resource_id, genre_id) VALUES (2,8);
+
+INSERT INTO genres_resources (resource_id, genre_id) VALUES (3,7);
+INSERT INTO genres_resources (resource_id, genre_id) VALUES (3,8);
+INSERT INTO genres_resources (resource_id, genre_id) VALUES (3,9);
+
+INSERT INTO genres_resources (resource_id, genre_id) VALUES (4,7);
+INSERT INTO genres_resources (resource_id, genre_id) VALUES (4,10);
+
