5e0fa93abe614c782cb03eee6c714fd89e621f4c
Johannes Weber <johannes@drweber.de>
2022-05-09 12:51:58 -0700

    add user route handlers

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,39 @@
 const router = require("express").Router();
+const helpers = require("../modules/helpers");
 
-module.exports = db => {
+module.exports = (db) => {
+
+  const users = require("../modules/users")(db);
 
   router.get("/users", (req, res) => {
+    users.getAll()
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => {
+        res.status(500).send(err);
+      });
+  });
+
+  router.get("/users/:id", (req, res) => {
+    const id = helpers.sanitizeId(req.params.id);
+    users.getOne(id)
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => {
+        res.status(500).send(err);
+      });
+  });
+
+  router.post("/users", (req, res) => {
+    users.createNew(req.body)
+      .then(result => {
+        res.json(result);
+      })
+      .catch(err => {
+        res.status(500).send(err);
+      });
   });
 
   return router;
