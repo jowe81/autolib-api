50ca9cc43eb46c5f0de43f017ff4cb23940fa608
Johannes Weber <johannes@drweber.de>
2022-05-09 15:01:36 -0700

    add login, logout, register routes

---
+++ src/routes/sessionRegister.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const helpers = require("../modules/helpers");
+
+module.exports = (db) => {
+
+  const users = require("../modules/users")(db);
+
+  router.post("/login", (req, res) => {
+    const email = req.body.email;
+    users.findByEmail(email)
+      .then(user => {
+        req.session.user = user;
+        res.json(user);
+      })
+      .catch(err => {
+        res.status(500).send(err);
+      });
+  });
+
+  router.get("/logout", (req, res) => {
+    const firstName = req.session.user.first_name;
+    res.end(`Goodbye, ${firstName}!`);
+    req.session = null;
+  });
+
+  router.post("/register", (req, res) => {
+    res.end("POST /register route");
+  });
+
+
+  return router;
+};
