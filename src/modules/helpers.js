c4adde200282389df22d5a2a811919a785eef383
Johannes Weber <johannes@drweber.de>
2022-05-10 22:06:44 -0700

    add day, daysBetween(), addDays()

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -19,6 +19,30 @@ const lowerAndEscape = s => {
   return s.replaceAll(`'`, `''`).toLowerCase();
 };
 
+const day = 1000 * 60 * 60 * 24;
+
+/**
+ * Return the number of full days between two dates
+ * @param {Date} date1
+ * @param {Date} date2
+ * @returns a positive integer representing the full days between the two dates
+ */
+const daysBetween = (date1, date2) => {
+  const difference = Math.abs(date1.getTime() - date2.getTime());
+  const daysBetween = Math.floor(difference / day);
+  return daysBetween;
+};
+
+/**
+ * Return the date that is a given number of days away from the reference date
+ * @param {Date} date reference date
+ * @param {integer} days number of days
+ * @returns Date
+ */
+const addDays = (date, days) => {
+  return new Date(date.getTime() + days * day);
+};
+
 /**
  * Log to the console if in development mode
  * @param {*} msg
@@ -32,5 +56,7 @@ const lg = (msg) => {
 module.exports = {
   sanitizeId,
   lowerAndEscape,
+  daysBetween,
+  addDays,
   lg,
 };
