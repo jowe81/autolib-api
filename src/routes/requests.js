beba22443fc80e64e31e40fecfff3c54224219be
Johannes Weber <johannes@drweber.de>
2022-05-11 16:18:28 -0700

    test and fix post route handler

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -1,34 +1,44 @@
 const router = require("express").Router();
 const errorIfUnauthorized = require("../middleware/errorIfUnauthorized");
 const helpers = require("../modules/helpers");
-const resources = require("./resources");
 
 module.exports = (db) => {
 
   const requests = require("../modules/requests")(db);
+  const resources = require("../modules/resources")(db);
 
   router.post("/requests", errorIfUnauthorized, (req, res) => {
     const userId = req.session.user.id;
     const resourceId = req.body.resourceId;
-    if (resourceId) {
-      resources.getStatus(resourceId)
-        .then(status => {
-          if (status.available) {
-            requests.createNew(resourceId, userId)
-              .then(requestRecord => {
-                res.json(requestRecord);
-              })
-              .catch(err => {
-                res.status(500).end(err);
-              });
-          } else {
-            res.json({});
-          }
-        })
-        .catch(err => {
-          res.status(500).end(err);
-        });
-    }
+    resources.exists(resourceId)
+      .then(exists => {
+        if (exists) {
+          resources.getStatus(resourceId)
+            .then(status => {
+              if (status.available) {
+                requests.createNew(resourceId, userId)
+                  .then(requestRecord => {
+                    res.json(requestRecord);
+                  })
+                  .catch(err => {
+                    res.status(500).end(err);
+                  });
+              } else {
+                helpers.lg(`Cannot create request.`);
+                res.json({ "error": "resource unavailable"});
+              }
+            })
+            .catch(err => {
+              res.status(500).end(err);
+            });
+        } else {
+          helpers.lg(`Resource with id ${resourceId} does not exist.`);
+          res.json({ "error": "resource does not exist"});
+        }
+      })
+      .catch(err => {
+        res.status(500).end(err);
+      });
   });
 
   router.put("/requests/:id", errorIfUnauthorized, (req, res) => {
