ac4cbcde8302bb74e7705e88ba47e8bdab9eacd7
Johannes Weber <johannes@drweber.de>
2022-05-11 18:22:19 -0700

    add console logging

--- src/middleware/errorIfUnauthorized.js
+++ src/middleware/errorIfUnauthorized.js
@@ -1,5 +1,8 @@
+const helpers = require("../modules/helpers");
+
 const errorIfUnauthorized = (req, res, next) => {
   if (!(req.session && req.session.user)) {
+    helpers.lg(`Unauthorized request (must be logged in).`);
     res.status(401).end("Not authorized - must be logged in.");
   } else {
     next();
