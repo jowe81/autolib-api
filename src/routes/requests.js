8460d09883cd7ccef2cbd9d17132148dc071159e
Johannes Weber <johannes@drweber.de>
2022-05-11 17:22:42 -0700

    implement PUT /requests/:id/complete route handler

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -7,6 +7,35 @@ module.exports = (db) => {
   const requests = require("../modules/requests")(db);
   const resources = require("../modules/resources")(db);
 
+  router.put("/requests/:id/complete", errorIfUnauthorized, (req, res) => {
+    const id = helpers.sanitizeId(req.params.id);
+    requests.getOne(id)
+      .then(requestRecord => {
+        if (requestRecord.requester_id === req.session.user.id) {
+          requests.markCompleted(id)
+            .then(requestRecord => {
+              helpers.lg(`Marked request ${id} as completed.`);
+              res.json(requestRecord);
+            })
+            .catch(err => {
+              res.status(500).end(err);
+            });
+        } else {
+          if (requestRecord.id) {
+            helpers.lg(`User ${req.session.user.id} tried to update a request they do not own.`);
+            res.status(500).end(`You cannot update a request you do not own.`);
+          } else {
+            helpers.lg(`A request with ID ${id} does not exist.`);
+            res.status(500).end(`Attempt to update a request record that does not exist.`);
+          }
+        }
+      })
+      .catch(err => {
+        helpers.lg(`Unable to retrieve request with ID ${id}.`);
+        res.status(500).end(err);
+      });
+  });
+
   router.post("/requests", errorIfUnauthorized, (req, res) => {
     const userId = req.session.user.id;
     const resourceId = req.body.resourceId;
@@ -30,10 +59,6 @@ module.exports = (db) => {
       });
   });
 
-  router.put("/requests/:id", errorIfUnauthorized, (req, res) => {
-    const id = helpers.sanitizeId(req.params.id);
-    //update request
-  });
 
   return router;
 };
