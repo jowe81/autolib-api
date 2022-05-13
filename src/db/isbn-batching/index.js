cb8a33b086809f4b0a335aeb6b6d801f096b71a3
Johannes Weber <johannes@drweber.de>
2022-05-13 10:36:10 -0700

    implement OL batch retrieval by ISBN

--- src/db/isbn-batching/index.js
+++ src/db/isbn-batching/index.js
@@ -3,7 +3,8 @@
   query OpenLibrary and add resource records to database.
 */
 
-const isbnFile = process.argv[2];
+const isbnDir = process.argv[2]; //Directory with ISBN text files
+const noUsers = process.argv[3] || 8; //Number of users for randomly assigning owners and possessors
 
 const ENV = require("../../environment");
 const dotenv = require('dotenv');
@@ -13,38 +14,52 @@ const db = require("../../db");
 const resources = require("../../modules/resources")(db);
 const ol = require("../../modules/openlibrary");
 const fs = require('fs');
+const path = require('path');
+const { rejects } = require("assert");
+console.log(`Attempting to read ${isbnDir}...`);
 
-console.log(`Attempting to read ${isbnFile}...`);
 
-fs.readFile('./isbns.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.log("Error!", err);
-  } else {
-    const isbns = data.split(/\r?\n/);
-    const promisesOL = [];
-    isbns.forEach(isbn => {
-      promisesOL.push(ol.getAutolibRecord(isbn));
-    });
-    Promise.all(promisesOL)
-      .then(autolibRecords => {
-        const promisesInsert = [];
-        autolibRecords.forEach(autolibRecord => {
-          promisesInsert.push(resources.createNew(autolibRecord));
-        });
-        Promise.all(promisesInsert)
-          .then(results => {
-            results.forEach(record => {
-              console.log(`Added book "${record.title}" by ${record.authors} - Resource ID: ${record.id}`);
-            });
-            console.log(`Inserted ${results.length} records into the database.`);
-            console.log(`CTRL+C to exit.`);
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+fs.readdir(isbnDir, (err, files) => {
+  files.forEach(file => {
+    let genre = path.basename(file,'.txt');
+    genre = genre[0].toUpperCase() + genre.substring(1);
+    processIsbnFile(file, genre);
+  });
 });
+
+const randomInt = max => {
+  return Math.ceil(Math.random() * max);
+};
+
+const processIsbnFile = (filename, genre) => {
+  console.log(`Processing ${filename}, genre ${genre}...`);
+  fs.readFile(isbnDir + '/' + filename, 'utf8', (err, data) => {
+    if (err) {
+      console.log("Error!", err);
+    } else {
+      const isbns = data.split(/\r?\n/);
+      const promisesOL = [];
+      isbns.forEach(isbn => {
+        promisesOL.push(ol.getAutolibRecord(isbn));
+      });
+      Promise.allSettled(promisesOL)
+        .then(values => {
+          values.forEach((values) => {
+            const status = values.status;
+            const autolibRecord = values.value;
+            if (status === 'fulfilled') {
+              autolibRecord.genres = genre;
+              autolibRecord.current_possessor_id = randomInt(noUsers);
+              resources.createNew(autolibRecord, randomInt(noUsers))
+                .then(record => {
+                  console.log(`Added book "${record.title}" by ${record.authors}, genre ${genre}. resource ID: ${record.id}`);
+                })
+                .catch(err => console.log("Failed to insert record!", err));
+            } else {
+              console.log(`Failed to retrieve book record from OpenLibrary`);
+            }
+          });
+        });
+    }
+  });
+};
