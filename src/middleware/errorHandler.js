b49da43dd91dc3674064f4e5942859107cbb5d2b
Johannes Weber <johannes@drweber.de>
2022-05-22 20:01:28 -0700

    ensure statusCode property gets to browser

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -3,7 +3,8 @@ const helpers = require("../modules/helpers");
 const errorHandler = (err, req, res, next) => {
   const debugMessage = err.debug ? `${err.debug} -` : ``;
   helpers.lg(`ERROR: ${debugMessage} ${err.message}`);
-  res.status(err.statusCode || 500).send(err);
+  err.statusCode = err.statusCode || 500;
+  res.status(err.statusCode).send(err);
 };
 
 module.exports = errorHandler;
